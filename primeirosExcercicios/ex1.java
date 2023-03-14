6d46c253fec52f3ca7be86dfb194cf75f906393f
albmarques <112758494+albmarques@users.noreply.github.com>
2023-03-14 09:31:18 -0300

    Update and rename we to ex1.java

---
+++ primeirosExcercicios/ex1.java
@@ -0,0 +1,11 @@
+String  texto="";
+        
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                texto +=j * i;
+                texto +=" | ";
+            }
+            
+            texto+="\n";
+            
+        }
